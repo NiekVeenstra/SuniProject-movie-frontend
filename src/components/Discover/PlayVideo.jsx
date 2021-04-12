f23a27a4fc79d9e76621753d84f560827d107a60
Niek <n.veenstra02@gmail.com>
2021-04-12 16:37:43 +0200

    adding autop play option

--- src/components/Discover/PlayVideo.jsx
+++ src/components/Discover/PlayVideo.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactPlayer from "react-player/youtube";
 
 const PlayVideo = () => {
+  const [mute, setMute] = useState(true);
+
   return (
     <div className="play-video">
       <div className="play-video__container">
@@ -10,10 +12,11 @@ const PlayVideo = () => {
           width="100%"
           height="100%"
           playing={true}
-          // muted={mute}
+          muted={mute}
           controls={true}
           loop={true}
           url="https://www.youtube.com/embed/odM92ap8_c0"
+          onLoad={() => setMute(!mute)}
         ></ReactPlayer>
       </div>
     </div>
