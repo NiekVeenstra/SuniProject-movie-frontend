aa23382acabb56b4a19a27e2b074fa9628d93de3
Niek <n.veenstra02@gmail.com>
2021-04-12 14:42:39 +0200

    ..

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import {withRouter} from "react-router"
-import Footer from './components/Footer/Footer';
-
+import { withRouter } from "react-router";
+import Footer from "./components/Footer/Footer";
 
 // components
 import Navbar from "./components/Navbar/NavBar";
@@ -11,14 +10,13 @@ import PlayVideo from "./components/Discover/PlayVideo";
 // pages
 import HomePage from "./pages/HomePage";
 
-const App = withRouter(({location}) => {
+const App = withRouter(({ location }) => {
   return (
     <div>
-      {
-        location.pathname != "/watch" && <Navbar />
-      }
+      {location.pathname != "/watch" && <Navbar />}
       <Route exact path="/" component={HomePage} />
       <Route exact path="/watch" component={PlayVideo} />
+      {location.pathname != "/watch" && <Footer />}
     </div>
   );
 });
