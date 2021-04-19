207247e8c254da413c595a3d84f7207202cbc93a
Future.Sayf <thefuture.sayf@gmail.com>
2021-04-19 11:45:14 +0200

    MovieInfo styled

--- src/components/Discover/MovieInfo/VideoPreviewText.jsx
+++ src/components/Discover/MovieInfo/VideoPreviewText.jsx
@@ -6,25 +6,25 @@ const VideoPreviewText = () => {
             
             <div className="videoPreviewText__1">
                 <div className="videoPreviewText__1-1">
-                    <p>97% match 2020 #ICON #ICON #TimeMovie</p>
-                    <h4>#icon nr.8 in films vandaag</h4>
+                    <br/><p><span>97% match </span>2020 #ICON #ICON #TimeMovie</p>
+                    <h2>#icon nr.8 in films vandaag</h2><br/>
                     <p id="SamenvattingFilm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius porro culpa autem nihil. Sed voluptate expedita nisi assumenda id veniam, aliquam unde reiciendis. Magnam animi labore reiciendis, qui ut fuga.
                     </p>
                 </div>
                 <div className="videoPreviewText__1-2">
-                    <p>CAST informatie, Brad Pitt, Goldie, Hawn, Acteur</p>
-                    <p>Genre: speelfilms voor het hele gezin, familie komedie</p>
-                    <p>Deze titel is: meeslepend</p>
+                    <br/><p><span>Cast:</span> informatie, Brad Pitt, Goldie, Hawn, Acteur</p>
+                    <p><span>Genres:</span> speelfilms voor het hele gezin, familie komedie</p>
+                    <p><span>Deze film is:</span> meeslepend</p>
                 </div>
             </div>
             <div className="videoPreviewText__2">
-                    <h2>Over the Christmas Chronicle: Deel 2</h2>
-                    <p>Regiseur: API</p>
-                    <p>CAST: API</p>
-                    <p>SCHRIJVER: api</p>
-                    <p>GENRE: API</p>
-                    <p>DEZE TITEL IS: MEESLEPAND, FEELGOOD</p>
-                    <p>LEEFTIJDCLASSIFICATIE: #ICON #ICON kan angst en geweld bevatten.</p>
+                    <h1>Over the Christmas Chronicle: Deel 2</h1><br/>
+                    <p><span>Regiseur:</span> API</p>
+                    <p><span>CAST:</span> API</p>
+                    <p><span>SCHRIJVER:</span> api</p>
+                    <p><span>GENRE:</span> API</p>
+                    <p><span>DEZE TITEL IS:</span> MEESLEPAND, FEELGOOD</p>
+                    <p><span>LEEFTIJDCLASSIFICATIE:</span> #ICON #ICON kan angst en geweld bevatten.</p>
 
             </div>
         </div>
